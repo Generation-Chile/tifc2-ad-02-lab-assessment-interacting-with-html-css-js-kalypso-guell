993bd498a82d1234e0fd0d2a78e63863c4866d23
kalypso-guell <kalypsoguell@gmail.com>
2025-09-03 17:22:16 +0000

    entrega lab 03-09 kalypso

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("adios").innerHTML = "adios";
+document.getElementById("red").style.color = "orange";
+
+function colorBrown () {
+    document.getElementById("marron").style.color="brown";
+}
+
